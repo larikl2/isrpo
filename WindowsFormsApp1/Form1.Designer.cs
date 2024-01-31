81a5275bbf3ec0cb2887dfed835bc38249993222
unknown <429195-15>
2024-01-31 11:12:22 +0300

    laba3

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(167, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 95);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сформировать документ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Поставщик";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Покупатель";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(358, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Заказ №";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(439, 41);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(361, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(429, 163);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 321);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Word.Application winword = new Word.Application();
+        Word.Document document;
+        double ResPrice = 0;
+        List<double> ShopLinePrice = new List<double>();
+        List<Commodity> Shop = new List<Commodity>()
+        {
+            new Commodity(){ Id = "1", Product = "Апельсины", Count = "50", Price = "120"},
+            new Commodity(){ Id = "2", Product = "Арбузы", Count = "95", Price = "230"},
+            new Commodity(){ Id = "3", Product = "Тыква", Count = "140", Price = "150"},
+            new Commodity(){ Id = "4", Product = "Кокосы", Count = "15", Price = "210"},
+            new Commodity(){ Id = "5", Product = "Яблоки", Count = "200", Price = "15"},
+            new Commodity(){ Id = "6", Product = "Киви", Count = "100", Price = "125"}
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            document = winword.Documents.Add();
+            label4.Text = "Дата: " + DateTime.Now.ToString("dd.MM.yyyy");
+            foreach (Commodity com in Shop)
+            {
+                double summ = Convert.ToInt32(com.Count) * Convert.ToInt32(com.Price);
+                ShopLinePrice.Add(summ);
+                ResPrice += summ;
+            }
+            label5.Text = "Итого: " + ResPrice;
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.RowCount = Shop.Count;
+            dataGridView1.Columns[0].HeaderText = "ID";
+            dataGridView1.Columns[1].HeaderText = "Продукт";
+            dataGridView1.Columns[2].HeaderText = "Цена";
+            dataGridView1.Columns[3].HeaderText = "Количество";
+            dataGridView1.Columns[4].HeaderText = "Сумма";
+            for (int i = 0; i < Shop.Count; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = Shop[i].Id;
+                dataGridView1.Rows[i].Cells[1].Value = Shop[i].Product;
+                dataGridView1.Rows[i].Cells[2].Value = Shop[i].Price;
+                dataGridView1.Rows[i].Cells[3].Value = Shop[i].Count;
+                dataGridView1.Rows[i].Cells[4].Value = ShopLinePrice[i];
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Word.Paragraph invoicePar = document.Content.Paragraphs.Add();
+            DateTime? selectDate = DateTime.Now;
+            string invoiceNumber = textBox3.Text;
+            if (selectDate != null)
+            {
+                invoicePar.Range.Text = string.Concat("Расходная накладная № ", invoiceNumber, " от ", selectDate.Value.ToString("dd.MM.yyyy"));
+            }
+            else
+            {
+                invoicePar.Range.Text = string.Concat("Расходная накладная № ", invoiceNumber);
+            }
+            invoicePar.Range.Font.Name = "Times new roman";
+            invoicePar.Range.Font.Size = 14;
+            invoicePar.Range.InsertParagraphAfter();
+
+            string PurchasertxtBox = textBox1.Text;
+            Word.Paragraph providerPar = document.Content.Paragraphs.Add();
+            providerPar.Range.Text = string.Concat("Поставщик: ", PurchasertxtBox);
+            providerPar.Range.Font.Name = "Times new roman";
+            providerPar.Range.Font.Size = 14;
+            providerPar.Range.InsertParagraphAfter();
+
+            Word.Paragraph customerPar = document.Content.Paragraphs.Add();
+            string ProvidertxtBox = textBox2.Text;
+            customerPar.Range.Text = "Покупатель: " + ProvidertxtBox;
+            customerPar.Range.Font.Name = "Times new roman";
+            customerPar.Range.Font.Size = 14;
+            customerPar.Range.InsertParagraphAfter();
+
+
+            int nRows = Shop.Count;
+            Word.Table myTable = document.Tables.Add(customerPar.Range, nRows, 5);
+            myTable.Borders.Enable = 1;
+            for (int i = 1; i < Shop.Count + 1; i++)
+            {
+                var dataRow = myTable.Rows[i].Cells;
+                dataRow[1].Range.Text = Shop[i - 1].Id.ToString();
+                dataRow[2].Range.Text = Shop[i - 1].Product;
+                dataRow[3].Range.Text = Shop[i - 1].Count.ToString();
+                dataRow[4].Range.Text = Shop[i - 1].Price.ToString();
+                dataRow[5].Range.Text = ShopLinePrice[i - 1].ToString();
+            }
+            Word.Paragraph resPar = document.Content.Paragraphs.Add();
+            resPar.Range.Text = string.Concat("Итого:", ResPrice);
+            resPar.Range.Font.Name = "Times new roman";
+            resPar.Range.Font.Size = 14;
+            resPar.Range.InsertParagraphAfter();
+
+            object filename = @"H:\wordExample.doc";
+            document.SaveAs(filename);
+            document.Close();
+            winword.Quit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Commodity
+    {
+        public string Id;
+        public string Product;
+        public string Count;
+        public string Price;
+    }
+
+}
